3cc7c3b200097e97bb3c83825b86090dd64fa54c
XHHuiL <16302010048@fudan.edu.cn>
2018-03-28 17:18:30 +0800

    remove package and logger

--- Main.java
+++ Main.java
@@ -19,12 +19,6 @@ public class Main extends Application {
     private double stageY;
 
     public static void main(String[] args) {
-        /*
-        System.out.println(DateUtil.getCurrentYear());
-        System.out.println(DateUtil.getCurrentMonth());
-        System.out.println(DateUtil.getCurrentDay());
-        */
-        //todo  We will run this class to test your codes.
         launch(args);
     }
 
